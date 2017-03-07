f7d59ef94ff48b4bd406a0d31ae1308939c41041
Pavel <Casear4000@gmail.com>
2017-03-07 18:57:57 +0300

    Remake

--- C/DZ#2/main.c
+++ C/DZ#2/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 #include <memory.h>
 #include <errno.h>
 #include "matrix.h"
@@ -15,39 +14,19 @@ int main(int argc, char *argv[]) {
     FILE *fp = fopen(path, "r");
     if (fp == NULL) {
         printf("Unable to open %s: %s.\n", path, strerror(errno));
-        return EXIT_FAILURE;
-    }
-
-    int rows, cols;
-    assert(fscanf(fp, "%d %d", &rows, &cols) == 2);
-
-    double **matrix = malloc(sizeof(double *) * rows);
-    if (!matrix) {
-        printf("Can't allocate %zu bytes: %s.\n", sizeof(double *) * rows, strerror(errno));
-        free(matrix);
         fclose(fp);
         return EXIT_FAILURE;
     }
-    for (int i = 0; i < rows; ++i) {
-        matrix[i] = calloc(sizeof(double), (size_t) cols);
-        assert(matrix[i]);
+
+    Matrix *matrix = create_matrix_from_file(fp);
+    if (!matrix) {
+        fclose(fp);
+        return EXIT_FAILURE;
     }
 
-    int row, col, num;
-    double value;
-    assert(fscanf(fp, "%d", &num) == 1);
-    for (int i = 0; i < num; ++i) {
-        assert(fscanf(fp, "%d %d %lf", &row, &col, &value) == 3);
-        set_elem(matrix, row, col, value);
-    }
-
-    transpose((const double **) matrix, cols, rows);
-
-    for (int i = 0; i < rows; ++i) {
-        free(matrix[i]);
-    }
-    free(matrix);
+    print_matrix(matrix);
+    transpose(matrix);
+    free_matrix(matrix);
     fclose(fp);
     return EXIT_SUCCESS;
 }
-
